c6e69a17b58a7c3e0f0a5de73166f5103da3f743
Pawel <pawelrugor26@wp.pl>
2020-11-15 06:09:09 +0100

    Plots upgrade

--- computations_backend/src/plots_gen/PlotsGenerator.py
+++ computations_backend/src/plots_gen/PlotsGenerator.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from io import BytesIO
 
+import matplotlib.pyplot as plt
+
 
 class PlotsGenerator:
 
@@ -22,13 +24,17 @@ class PlotsGenerator:
             ranges = data.get('ranges', None)
             for points_range in ranges:
                 df[label] = df.index.to_series().apply(
-                    lambda x: (idx * 0.2) + 1 if points_range['start'] <= x <= points_range['end'] else df.loc[x, label])
+                    lambda x: idx + 1 if points_range['start'] <= x <= points_range['end'] else df.loc[x, label])
 
         sns.set_theme()
         sns.set_context("poster")
-        p = df.plot(kind='line', colormap='tab20c', style=['-'] * len(df.columns), legend=False, linewidth=6)
-        p.set_yticks(range(1, len(columns) + 1))
-        p.set_yticklabels(columns)
+        p = df.plot(kind='line', ylim=[0, len(columns) + 1], xlim=[0, duration], colormap='tab20c', style=['-'] * len(df.columns), legend=False, linewidth=6, figsize=(12, 6))
+        p.set_yticks(range(1, (len(columns) + 1)))
+        p.set_yticklabels(columns, rotation=15, fontdict={'fontsize': 16})
+        p.set_xticklabels(np.arange(0, 31, 5), fontdict={'fontsize': 12})
+        p.set_title('Objects occurences', fontdict={'fontsize': 36}, pad=20)
+        p.set_xlabel('media time [s]', fontdict={'fontsize': 18})
+        plt.tight_layout()
         buf = BytesIO()
         p.figure.savefig(buf, format="png")
         return base64.b64encode(buf.getbuffer())
