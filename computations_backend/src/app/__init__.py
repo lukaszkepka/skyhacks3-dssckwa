7cf3371ceca0b1c738009cfa0786cb5d261906c4
Marcin Pszczółka <mpszczolka@future-processing.com>
2020-11-15 02:05:55 +0100

    enable CORS in Flask

--- computations_backend/src/app/__init__.py
+++ computations_backend/src/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 from speech_recognition.recognize import SpeechToTextAlgorithm
 
 app = Flask(__name__)
+CORS(app)
 
 mp3_file_path = 'C:\\Users\\Lukasz Kepka\\Downloads\\audio_all\\audio_all\\audio_tour\\69384\\pl\\625986.mp3'
 text_model_lite_path = '../model/wiki-lemmas-all-100-skipg-ns.txt'
